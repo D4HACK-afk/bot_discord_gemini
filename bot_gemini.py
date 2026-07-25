79079b39fb39eebf56cf3486a027bbb5adb36196
d4hack-afk <d4hack2308@gmail.com>
2026-07-25 16:37:41 -0400

    Api gemini

--- bot_gemini.py
+++ bot_gemini.py
@@ -1,7 +1,14 @@
 import discord 
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+from google import genai
 
 
-
+#*............................................................................................./
+load_dotenv()
+token_discord = os.getenv("token_bot")
+token_IA = os.getenv("token_gemini")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,9 +26,9 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+#*............................................................................................./
 
 
 
 
-
-client.run('your token here')
+client.run(token_discord)
